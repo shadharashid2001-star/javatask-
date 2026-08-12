cbd9e49aff8d23b5ca601bcb1ecbd4783110eaed
shadha <shadharashid2001@gmail.com>
2026-08-12 16:42:17 +0400

    uploade task 433

---
+++ task12/src/TASK433/TEXT.java
@@ -0,0 +1,9 @@
+package TASK433;
+
+public class TEXT {
+    //Interface: Tells the class what it should do.
+    //Abstract class: A parent class that can have methods and variables.
+    //A class can implement many interfaces, but can extend only one class.
+    //Diamond problem: When two parents have the same method, the child doesn't know which one to use.
+    //Java avoids this by allowing a class to extend only one class.
+}
